d3c8c6ddfa59b970448f25755ab9911be3d70ff5
matopc <spekov@gmail.com>
2024-05-12 11:20:31 +0200

    rework added

--- main.py
+++ main.py
@@ -6,62 +6,48 @@ import cv2
 import numpy as np
 
 
+#import pose estimation model
 mp_pose = mp.solutions.pose
+#for drawing skeleton
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
 
-
 if __name__ == '__main__':
-    poselandmarks_list = []
-
     #file = 'man_squatting_720p.mp4'
     file = 'man_running.mp4'
+    show_only_skeleton = True
+    cap = cv2.VideoCapture(file)
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-        capture = cv2.VideoCapture(file)
+        while cap.isOpened():
+            ret, frame = cap.read()
 
-        if not capture.isOpened():
-            print("Error opening video file")
-            raise TypeError
+            image = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
 
-        frame_width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
-        frame_height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        fps = capture.get(cv2.CAP_PROP_FPS)
-        frames = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
-
-        # Create a NumPy array to store the pose data as before
-        # The shape is 3x33x144 - 3D XYZ data for 33 landmarks across 144 frames
-        data = np.empty((2, 33, frames))
-
-        frame_num = 0
-
-        size = (frame_width, frame_height)
-
-        while capture.isOpened():
-            ret, image = capture.read()
-            if not ret:
-                break
-
-            image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-            annotated_image = image.copy()
             results = pose.process(image)
+            image.flags.writeable = True
+            image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
 
-            mp_drawing.draw_landmarks(annotated_image,
+            try:
+                landmarks = results.pose_landmarks.landmark
+                print(landmarks[mp_pose.PoseLandmark.LEFT_HEEL.value])
+            except:
+                pass
+
+            if show_only_skeleton:
+                image[: , :] = (0, 0, 0)
+
+            mp_drawing.draw_landmarks(image,
                                       results.pose_landmarks,
                                       mp_pose.POSE_CONNECTIONS,
-                                      landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+                                      mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2),
+                                      mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2))
 
-            landmarks = results.pose_world_landmarks.landmark
-            for i in range(len(mp_pose.PoseLandmark)):
-                data[:, i, frame_num] = (landmarks[i].x, landmarks[i].y)
+            cv2.imshow('Mediapipe feed', image)
 
-            frame_num += 1
-
-            cv2.imshow('Frame', annotated_image)
-            # Press Q on keyboard to  exit
-            if cv2.waitKey(25) & 0xFF == ord('q'):
+            if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
 
-        # Close the video file
-        capture.release()
+        cap.release()
         cv2.destroyAllWindows()
